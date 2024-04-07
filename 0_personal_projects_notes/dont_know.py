9de7de7a400b202ed20d8655d5c79f947fdf0ae0
emeka.enshinyan <emeka.enshinyan@gmail.com>
2024-04-07 14:41:40 +1000

    add: division; dont_know

---
+++ 0_personal_projects_notes/dont_know.py
@@ -0,0 +1,10 @@
+'''
+the following are either 
+1. terms I do not know/understand
+2. features/methods I don't know how to manipulate
+3. both 1. and 2.
+'''
+
+# hashable
+# dunder methods
+# dictionaries
---
+++ 9136_python/A1/division.py
@@ -0,0 +1,67 @@
+#NOTE: Python, by default, rounds down and thus must be coded to 
+    #round up if needing to
+
+#test operands
+p1 = 1
+p5 = 5
+p7 = 7
+p11 = 11
+
+e2 = 2
+e4 = 4
+e6 = 6
+e8 = 8
+
+f2 = 2.0
+f5 = 5.0
+f15p = 15.343579
+f20p = 20.99988
+
+# store the values of above vars into a list
+variable_names = dir()
+
+values_li = []
+
+for name in variable_names:
+    if not name.startswith('__') and not callable(eval(name)):
+        values_li.append(eval(name))
+
+# Print the list of values
+print("List of values:", values_li)
+
+
+
+dict_divisors = {}
+print("dict", dict_divisors)
+if len(dict_divisors) == 0:
+    pass
+        
+    
+
+def get_modulo(x,y):
+    return x % y
+
+def get_float_quotient(x,y) -> float:
+    return x / y
+
+def get_floor_quotient(x,y) -> int:
+    return x // y
+
+
+
+#interprete f(int, int)
+'''
+print(get_modulo(e8, e2))
+print(get_modulo(e2, e8))
+print(get_float_quotient(e6,e2))
+print(get_float_quotient(e2,e6))
+print(get_floor_quotient(e8,e4))
+print(get_floor_quotient(e4,e8))
+#interpret f(int, float)
+print(get_modulo(e8, e2))
+print(get_modulo(e2, e8))
+print(get_float_quotient(e6,e2))
+print(get_float_quotient(e2,e6))
+print(get_floor_quotient(e8,e4))
+print(get_floor_quotient(e4,e8))
+'''
