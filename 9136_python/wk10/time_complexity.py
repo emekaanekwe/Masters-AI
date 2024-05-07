86f5f7104fa93ff04a7db23db4bc2c7f701007cc
eane0001 <eane0001@student.monash.edu>
2024-05-08 08:48:49 +1000

    xfer: transfered copy to monash-linked github account

---
+++ 9136_python/wk10/time_complexity.py
@@ -0,0 +1,16 @@
+'''Sorting Algorithms'''
+
+class Merge_Sort:
+    def __init__(self, input):
+        self.input = input
+    
+    def merge(left: list, right: list) -> list:
+        def merge():
+            pass
+
+'''pseudo code'''
+
+# take a list and start with an empty list
+    # check if element[0] < element[1]
+        # if so, then move on to [1] and [2]
+
