0818ea72d7c9c5c7d40668cbf31be417daa6ebdc
eane0001 <eane0001@student.monash.edu>
2024-05-14 13:03:33 +1000

    add: sort

---
+++ 9136_python/wk11/binarsearch.py
@@ -0,0 +1,19 @@
+'''STRUCTURE'''
+# cna be a linked like structure where each node
+    # is an obj
+'''PROPERTIES'''
+# nodes - holders of data
+    # left - points to left child where 
+        # for key.left <= key.right
+    # right - points to right child
+    # p - the determinant point
+'''OUTPUTTING'''
+# binary search via recurvie algo - inorder tree walk
+
+class BinasrySearch:
+    
+    def __init__(self,):
+        self.node = None
+    
+    def sort(self):
+        pass
